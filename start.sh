62665247ad4d88bbf5a4dd6bcbe2b1da061d80a6
DanProtiviti <51682987+DanProtiviti@users.noreply.github.com>
2019-08-29 23:08:27 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,4 +3,5 @@
 echo deb http://http.kali.org/kali kali-rolling main contrib non-free >> /etc/apt/sources.list
 echo deb-src http://http.kali.org/kali kali-rolling main contrib non-free >> /etc/apt/sources.list
 /etc/init.d/postgresql start
-/usr/share/metasploit-framework/msfconsole
+msfupdate
+
