8a455cb7c636ca5e361959075491cf597848377b
DanProtiviti <51682987+DanProtiviti@users.noreply.github.com>
2019-08-29 23:17:16 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,4 +4,6 @@ echo deb http://http.kali.org/kali kali-rolling main contrib non-free >> /etc/ap
 echo deb-src http://http.kali.org/kali kali-rolling main contrib non-free >> /etc/apt/sources.list
 /etc/init.d/postgresql start
 msfupdate
-
+mkdir /opt/git
+git clone https://github.com/OJ/gobuster.git /opt/git/
+git clone https://github.com/danielmiessler/SecLists.git /opt/git/
